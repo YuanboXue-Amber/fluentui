d27bb55c342483abdff3ddb656302e234b0dbc1c
YuanboXue-Amber <yuanboxue@microsoft.com>
2020-09-17 18:01:43 +0200

    Remove click preventDefault

--- packages/fluentui/react-northstar/src/components/Tree/TreeTitle.tsx
+++ packages/fluentui/react-northstar/src/components/Tree/TreeTitle.tsx
@@ -120,7 +120,7 @@ export const TreeTitle: ComponentWithAs<'a', TreeTitleProps> & FluentComponentSt
     debugName: TreeTitle.displayName,
     actionHandlers: {
       performClick: e => {
-        e.preventDefault();
+        // e.preventDefault();
         e.stopPropagation();
         handleClick(e);
       },
