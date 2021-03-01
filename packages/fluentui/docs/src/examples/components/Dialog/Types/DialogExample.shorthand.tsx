79f61e9bb28324edd208544344a8aeb092bfc9f6
YuanboXue-Amber <yuanboxue@microsoft.com>
2021-03-01 13:54:24 +0100

    wip

--- packages/fluentui/docs/src/examples/components/Dialog/Types/DialogExample.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Dialog/Types/DialogExample.shorthand.tsx
@@ -1,11 +1,22 @@
-import { Button, Dialog } from '@fluentui/react-northstar';
+import { Button, Dialog, Image } from '@fluentui/react-northstar';
 import * as React from 'react';
 
-const DialogExample: React.FC = () => (
+const content = `a`;
+
+const DialogExample = () => (
   <Dialog
     cancelButton="Cancel"
     confirmButton="Confirm"
     header="Action confirmation"
+    content={
+      <div>
+        {content}
+        <Image
+          src="https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CarolePoland.jpg"
+          style={{ height: '500px', width: '500px' }}
+        />
+      </div>
+    }
     trigger={<Button content="Open a dialog" />}
   />
 );
