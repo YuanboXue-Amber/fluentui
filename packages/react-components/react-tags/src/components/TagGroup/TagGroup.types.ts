2b229fcf9f4f44323c2e5c6820e69fe3554d5f55
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-05-17 16:06:21 +0200

    Revert "remove dismiss change"
    This reverts commit cfbc28a7204490eae164fa54d30ce7812264e334.

--- packages/react-components/react-tags/src/components/TagGroup/TagGroup.types.ts
+++ packages/react-components/react-tags/src/components/TagGroup/TagGroup.types.ts
@@ -1,18 +1,27 @@
+import * as React from 'react';
 import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
-import { TagSize } from '../Tag/Tag.types';
+import { TagProps, TagSize } from '../Tag/Tag.types';
 
 export type TagGroupSlots = {
   root: Slot<'div'>;
 };
 
+export type ItemRenderFunction<TItem = TagProps> = (item: TItem) => React.ReactNode;
+
 /**
  * TagGroup Props
  */
-export type TagGroupProps = ComponentProps<TagGroupSlots> & {
+export type TagGroupProps<TItem = TagProps> = Omit<ComponentProps<TagGroupSlots>, 'children'> & {
+  children: React.ReactNode | ItemRenderFunction<TItem>;
+  items?: Array<TItem>;
+  onDismiss?: (e: React.MouseEvent | React.KeyboardEvent, dismissedTagIds: string[]) => void;
   size?: TagSize;
 };
 
 /**
  * State used in rendering TagGroup
  */
-export type TagGroupState = ComponentState<TagGroupSlots> & Required<Pick<TagGroupProps, 'size'>>;
+export type TagGroupState = ComponentState<TagGroupSlots> &
+  Required<Pick<TagGroupProps, 'items' | 'size'>> & {
+    handleTagDismiss: (e: React.MouseEvent | React.KeyboardEvent, id: string) => void;
+  };
