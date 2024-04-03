5c5d0af04eaf82d07fa4a5abb5bc40714a2addd5
YuanboXue-Amber <yuanboxue@microsoft.com>
2024-04-03 17:14:08 +0200

    useHeadlessFlatVirtualizedTree_unstable

--- packages/react-components/react-tree/stories/Tree/TreeDefault.stories.tsx
+++ packages/react-components/react-tree/stories/Tree/TreeDefault.stories.tsx
@@ -3,10 +3,7 @@
 import * as React from 'react';
 import {
   FlatTreeProps,
-  FlatTreeItem,
   TreeItemLayout,
-  TreeProvider,
-  FlatTreeSlots,
   TreeNavigationData_unstable,
   TreeNavigationEvent_unstable,
   useFlatTree_unstable,
@@ -15,17 +12,18 @@ import {
   useFlatTreeContextValues_unstable,
   HeadlessFlatTreeItem,
   useHeadlessFlatTree_unstable,
-  useMergedRefs,
-  mergeClasses,
-  Slot,
-  ComponentProps,
-  ComponentState,
-  slot,
-  getIntrinsicElementProps,
-  FlatTreeState,
+  HeadlessFlatTreeOptions,
+  HeadlessFlatTree,
+  FlatTreeItemProps,
+  useTreeItem_unstable,
+  renderTreeItem_unstable,
+  useTreeItemContextValues_unstable,
+  useTreeItemStyles_unstable,
+  renderFlatTree_unstable,
+  useEventCallback,
 } from '@fluentui/react-components';
-import { ForwardRefComponent, assertSlots } from '@fluentui/react-components';
-import { VirtualizerOptions, useVirtualizer } from '@tanstack/react-virtual';
+import { ForwardRefComponent } from '@fluentui/react-components';
+import { VirtualItem, useVirtualizer } from '@tanstack/react-virtual';
 
 type ItemProps = HeadlessFlatTreeItemProps;
 
@@ -50,102 +48,147 @@ const defaultItems: ItemProps[] = [
   })),
 ];
 
-type FlatVirtualizedTreeSlot = FlatTreeSlots & {
-  // TODO ban children from container type
-  container: NonNullable<Slot<'div'>>;
-};
+export const FlatVirtualizedTreeItem: ForwardRefComponent<
+  FlatTreeItemProps & { virtualItemProps: Pick<VirtualItem, 'size' | 'start'> }
+> = React.forwardRef(({ virtualItemProps, ...props }, ref) => {
+  const state = useTreeItem_unstable(props, ref);
 
-type FlatVirtualizedTreeProps = FlatTreeProps &
-  ComponentProps<FlatVirtualizedTreeSlot> & {
-    children: React.ReactElement<typeof FlatTreeItem>[];
-  } & Pick<VirtualizerOptions<Element, Element>, 'estimateSize'>;
-
-type FlatVirtualizedTreeState = FlatTreeState & ComponentState<FlatVirtualizedTreeSlot>;
-
-const useFlatVirtualizedTree = (
-  props: FlatVirtualizedTreeProps,
-  ref: React.Ref<HTMLDivElement>,
-): FlatVirtualizedTreeState => {
-  const baseState = useFlatTree_unstable(props, ref);
-
-  const state = {
-    ...baseState,
-    components: { ...baseState.components, container: 'div' },
-    container: slot.always(props.container, { elementType: 'div' }),
+  // TODO makeStyle the static styles
+  state.root.style = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: `${virtualItemProps.size}px`,
+    transform: `translateY(${virtualItemProps.start}px)`,
+    ...state.root.style,
   };
 
-  // -------- begin virtualization
+  useTreeItemStyles_unstable(state);
+  const contextValues = useTreeItemContextValues_unstable(state);
+  return renderTreeItem_unstable(state, contextValues);
+});
 
-  const parentRef = React.useRef<HTMLDivElement>(null);
-  state.container.ref = useMergedRefs(state.container.ref, parentRef);
+export const FlatVirtualizedTree: ForwardRefComponent<FlatTreeProps> = React.forwardRef((props, ref) => {
+  const state = useFlatTree_unstable(props, ref);
 
-  state.root.children = state.root.children ?? [];
-  const rowVirtualizer = useVirtualizer({
-    count: state.root.children.length,
-    getScrollElement: () => parentRef.current,
-    estimateSize: props.estimateSize,
-  });
-
+  // TODO makeStyle the static styles
   state.root.style = {
-    ...state.root.style,
-    height: `${rowVirtualizer.getTotalSize()}px`,
     width: '100%',
     position: 'relative',
+    ...state.root.style,
   };
 
-  state.root.children = rowVirtualizer.getVirtualItems().map(virtualItem => (
-    <div
-      key={virtualItem.key}
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: `${virtualItem.size}px`,
-        transform: `translateY(${virtualItem.start}px)`,
-      }}
-    >
-      {state.root.children[virtualItem.index]}
-    </div>
-  ));
-
-  // TODO handleNavigation
-
-  // -------- end virtualization
-  return state;
-};
-
-export const FlatVirtualizedTree: ForwardRefComponent<FlatVirtualizedTreeProps> = React.forwardRef((props, ref) => {
-  const state = useFlatVirtualizedTree(props, ref);
   const contextValues = useFlatTreeContextValues_unstable(state);
   useFlatTreeStyles_unstable(state);
 
-  // return renderFlatTree_unstable(state, contextValues);
-  assertSlots<FlatVirtualizedTreeSlot>(state);
-  return (
-    <TreeProvider value={contextValues.tree}>
-      <state.container>
-        <state.root />
-      </state.container>
-    </TreeProvider>
-  );
+  return renderFlatTree_unstable(state, contextValues);
 });
+
+export function useHeadlessFlatVirtualizedTree_unstable<Props extends HeadlessFlatTreeItemProps>(
+  props: Props[],
+  {
+    virtualizerOptions,
+    ...options
+  }: HeadlessFlatTreeOptions & {
+    virtualizerOptions: Omit<Parameters<typeof useVirtualizer>[0], 'count'>;
+  },
+): HeadlessFlatTree<Props> &
+  Omit<ReturnType<typeof useVirtualizer>, 'getVirtualItems'> & {
+    getVirtualItems: () => (HeadlessFlatTreeItem<Props> & {
+      virtualItemProps: Pick<VirtualItem, 'size' | 'start'>;
+    })[];
+  } {
+  const headlessTree = useHeadlessFlatTree_unstable(props, options);
+
+  const items = React.useMemo(() => Array.from(headlessTree.items()), [headlessTree]);
+
+  const rowVirtualizer = useVirtualizer({
+    count: items.length,
+    ...virtualizerOptions,
+  });
+
+  const baseGetVirtualItems = rowVirtualizer.getVirtualItems;
+  const getVirtualItems = React.useCallback(
+    () =>
+      baseGetVirtualItems().map(virtualItem => ({
+        virtualItemProps: { size: virtualItem.size, start: virtualItem.start },
+        ...items[virtualItem.index],
+      })),
+    [baseGetVirtualItems, items],
+  );
+
+  /**
+   * Since navigation is not possible due to the fact that not all items are rendered,
+   * we need to scroll to the next item and then invoke navigation.
+   */
+  const handleNavigation = useEventCallback(
+    (event: TreeNavigationEvent_unstable, data: TreeNavigationData_unstable) => {
+      const nextItem = headlessTree.getNextNavigableItem(items, data);
+      if (!nextItem) {
+        return;
+      }
+      // if the next item is not rendered, scroll to it and try to navigate again
+      if (!headlessTree.getElementFromItem(nextItem)) {
+        event.preventDefault(); // preventing default disables internal navigation.
+        rowVirtualizer.scrollToIndex(nextItem.index);
+        // waiting for the next animation frame to allow the list to be scrolled
+        return requestAnimationFrame(() => headlessTree.navigate(data));
+      }
+    },
+  );
+
+  const baseGetTreeProps = headlessTree.getTreeProps;
+  const getTreeProps = React.useCallback(
+    () => ({
+      ...baseGetTreeProps(),
+      onNavigation: handleNavigation,
+    }),
+    [baseGetTreeProps, handleNavigation],
+  );
+
+  return {
+    ...headlessTree,
+    getTreeProps,
+    ...rowVirtualizer,
+    getVirtualItems,
+  };
+}
 
 const estimateSize = () => 35;
 export const Default = () => {
-  const headlessTree = useHeadlessFlatTree_unstable(defaultItems);
+  const parentRef = React.useRef<HTMLDivElement>(null);
+  const headlessTree = useHeadlessFlatVirtualizedTree_unstable(defaultItems, {
+    virtualizerOptions: {
+      estimateSize,
+      getScrollElement: () => parentRef.current,
+    },
+  });
+
+  const virtualItems = headlessTree.getVirtualItems();
 
   return (
-    <FlatVirtualizedTree
-      {...headlessTree.getTreeProps()}
-      aria-label="Flat Virtualized Tree"
-      estimateSize={estimateSize}
-      container={{ style: { height: '50vh', overflow: 'auto' } }}
-    >
-      {Array.from(headlessTree.items(), flatTreeItem => {
-        const treeItemProps = flatTreeItem.getTreeItemProps();
-        return <FlatTreeItem {...treeItemProps} />;
-      })}
-    </FlatVirtualizedTree>
+    <div ref={parentRef} style={{ height: '50vh', overflow: 'auto' }}>
+      {/* An empty Tree that has no children on 1st render won't have keyboard navigation initialized. */}
+      {virtualItems.length && (
+        <FlatVirtualizedTree
+          {...headlessTree.getTreeProps()}
+          aria-label="Flat Virtualized Tree"
+          style={{ height: `${headlessTree.getTotalSize()}px` }}
+        >
+          {virtualItems.map(flatTreeItem => {
+            const { getTreeItemProps, virtualItemProps } = flatTreeItem;
+            const treeItemProps = getTreeItemProps();
+            return (
+              <FlatVirtualizedTreeItem
+                key={treeItemProps.value}
+                virtualItemProps={virtualItemProps}
+                {...treeItemProps}
+              />
+            );
+          })}
+        </FlatVirtualizedTree>
+      )}
+    </div>
   );
 };
